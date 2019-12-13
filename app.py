b26897e2ef1569e7e0b6022ab5ba5052d0ce3c44
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-12-12 23:57:22 -0800

    users can update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def teas_index():
 @app.route('/teas/new')
 def teas_new():
     """Create a new tea list."""
-    return render_template('tea_new.html', tea = {})
+    return render_template('tea_new.html',  tea ={}, title ="create new")
 
 
 @app.route('/teas/<tea_id>')
@@ -42,11 +42,34 @@ def teas_submit():
     tea = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'image':request.form.get('image')
+        "price": request.form.get("price"),
+        "domain": request.form.get("domain")
     }
 
     teas.insert_one(tea).inserted_id
     return redirect(url_for('teas_index'))
+
+@app.route("/teas/<tea_id>/edit")
+def teas_edit(tea_id):
+	tea = teas.find_one({"_id" : ObjectId(tea_id)})
+	return render_template("teas_edit.html", tea = tea, title = "Edit Your Product")
+
+
+@app.route("/teas/<tea_id>", methods = ['POST'])
+def teas_update(tea_id):
+	updated_tea = {
+		"tea_name": request.form.get("tea_name"),
+		"description": request.form.get("description"),
+		"price": request.form.get("price"),
+        "domain": request.form.get("domain")
+
+	}
+
+	teas.update_one( {"_id" : ObjectId(tea_id)}, {"$set" : updated_tea})
+	return redirect(url_for("teas_show", tea_id = tea_id))
+
+
+
 
 
 
