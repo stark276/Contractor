496f816b3c969773d7dcff6d6c2e376a0c8cd901
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2019-12-16 16:02:40 -0800

    der

--- app.py
+++ app.py
@@ -106,3 +106,7 @@ def gallery():
 if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
+    
+# if __name__ == '__main__':
+#     app.run(debug=True)
